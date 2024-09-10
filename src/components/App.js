cbbf6220cdafcd6862b5e2039e7e9be82943c7e6
nkutete <nathankutete99@gmail.com>
2024-09-10 12:26:15 +0300

    Update App.js

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchButton from './SearchButton';
+import Map from './Map';
+import './styles/style.css';
+
+function App() {
+  return (
+    <Router>
+      <div id="app">
+        <Routes>
+          <Route path="/" element={<SearchButton />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
